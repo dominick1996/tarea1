8fae9d336d7bd34522bc41929781f57930ce8d0f
david carbajal criollo <riccc.jjjhhggg@gmail.com>
2016-03-03 13:06:57 -0500

    primer trabajo

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html> 
+<head> 
+<meta charset="utf-8">
+<link rel="style sheet" href="style.css">
+<meta name="description" content="esta es la pagina de david carbajal criollo"> 
+<meta name="dreywor" content="pagina de david en html"> 
+
+<title>carbajal.c</title> 
+</head> 
+<body background="12.jpg" bgcolor="white" text="white">
+
+	<center><table border="05" width="100%" bgcolor="white"><tr>
+</style.css>
+<center><table border="10" width="80%" bgcolor="white"><tr>
+<a href="index.html"> 
+<font color="white"><img alt="Ficcion"></a>
+<a href="ficcion.html"> 
+<font color="white"><img  alt="Real"></a> 
+<a href="real.html"> 
+<font color="white"><img  alt="Escalofriantes"></a> 
+<a href="escalofriante.html"> 
+<font color="white"><img alt="Macabros"></a> 
+<a href="macabros.html">
+<font color="white"><img alt="Espelusnante"></a> 
+
+<a href="1.jpg"><center><img src="1.jpg"></center> 
+
+<center><table border="10" width="80%" background="10.jpg" bgcolor="blue"> 
+<tr> 
+<td colspan="2"><center><h1><font color="red" face="Lucida Calligraphy">7 impactantes historias de terror que sucedieron en la vida real</font> </h1></center></td> 
+</tr> 
+<tr> 
+<td><center><h3><font color="blue/green">
+<center><table border="10" width="80%" background="2.jpg"  bgcolor="blue">
+
+<center><h3><font color="white">7. El hombre que intentó salvar a Lincoln:</p></h3><font color="white"></center>
+
+
+Aunque sabemos exactamente qué pasó cuando Abraham Lincoln fue asesinado, y también conocemos el
+ nombre del asesino, poco sabemos del hombre que intentó salvar al presidente. Henry Rathbone estaba
+  allí con su esposa cuando falló en su cometido: no pudo salvar a Lincoln y esto cambió su vida radicalmente.
+
+Rathbone nunca se recuperó mentalmente e intent&oacute; matar a sus tres hijos 18 años más tarde. Cuando 
+su esposa lo impidió, la apuñaló y luego intentó suicidarse. Cuando la policía lo encontró, el hombre 
+seguía repitiendo que había personas escondidas detrás de los cuadros. Vivió el resto de su vida en un asilo, 
+convencido de que alguien intentaba envenenarlo a través de las paredes.
+<a href="3.jpg"><center><img src="3.jpg"></center>
+
+
+<tr>
+<tr>
+<td><center><h3><font color="blue/green">
+<center><h3><font color="white">6. Los zombies del sífilis en las calles de Italia:</p></h3><font color="white"></center>
+
+
+
+En 1494 Italia no se parecía en nada a la que conocemos hoy en día. Un enorme brote de sífilis asolaba la ciudad, y
+ la enfermedad no era escondida con vergüenza, como sucedería años más tarde, sino que se sufría frente a todo el mundo.
+
+La enfermedad causaba que la piel en el rostro de las personas se cayera, as&iacute; como las extremidades y los genitales. 
+En términos estéticos, era prácticamente como vivir en una película de zombies. Las personas morían en el correr de 
+algunos meses, con su piel completamente destruida.
+<a href="5.jpg"><center><img src="5.jpg"></center>
+
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white">5. Herculano y el Vesubio:</p></h3><font color="white"></center>
+
+
+Cuando pensamos en el Vesubio automáticamente pensamos en Pompeya, quizás por haber sido una de las ciudades
+ más importantes y esta ahora enterrada debajo de la ceniza. Sin embargo, Herculano fue una ciudad pequeña, 
+ mucho más cercana al Vesubio y con consecuencias peores. Se dice que incluso algunos cráneos explotaron debido 
+ al calor concentrado y el magma cayendo en la ciudad. Estar allí durante la erupción seguro fue un infierno, 
+ con todos los elementos del Inframundo.
+<a href="14.jpg"><center><img src="14.jpg"></center> 
+
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white">4. La primera asesina serial:</p></h3><font color="white"></center>
+Aunque no lo creas, el primer registro de alguien que asesinara gente en grandes cantidades fue una mujer en Roma
+ llamada Locusta, precisamente en un período pacífico de la ciudad. En el siglo I fue encarcelada por envenenamiento, 
+ pero Agripina le dio el perdón luego de que la ayudara a asesinar al Emperador Claudius. Lo mismo sucedió tiempo después 
+ con Nerón, quien además le dio un sitio para enseñar sus artes y convertirse en profesora. Sin embargo, no pudo salvarse 
+ cuando el Emperador Galba la condenó y fue ejecutada públicamente.
+
+<a href="6.jpg"><center><img src="6.jpg"></center>
+
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white">3. Chucky realmente existió:</p></h3><font color="white"></center>
+La película Child’s Play, donde un niño tiene un muñeco con un espíritu maligno dentro, est&aacute; basada en una historia 
+real. Robert es un muñeco de tamaño real que pertenecía al niño Gene Otto. Al parecer, un sirviente se enojó con la 
+familia y puso un hechizo sobre el muñeco. El niño comenzó a hablar con él y para sorpresa de todos, recibía respuesta. 
+Sin embargo, pocos creyeron en lo que Gene contaba sobre que el muñeco movía objetos. Luego de su muerte, Robert pasó de
+ mano en mano y ahora está en un museo donde, según cuentan, suceden cosas extrañas.
+
+<a href="7.jpg"><center><img src="7.jpg"></center>
+
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white">2. La canción que mata personas:</p></h3><font color="white"></center>
+En los años 30 se popularizó la canción Gloomy Sunday, pero poco a poco fue tornándose terrorífica
+ cuando se difundió que más de 18 personas (algunos dicen incluso 100) se suicidaron en Budapest debido
+  a ella. Hungría siempre tuvo altas tasas de suicidio, y la canción era tan popular que no resulta extraño
+   que un suicida la estuviera escuchando. Además, la letra depresiva no ayuda demasiado para mejorar el humor.
+    Sin embargo, hubo un suicidio real por ella: el creador de la canción se suicidó luego de que su único éxito 
+    fuera culpado de estas calamidades.
+<a href="8.jpg"><center><img src="8.jpg"></center>
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white">1. Los alpinistas desaparecidos:</p></h3><font color="white"></center>
+En 1959, un grupo de amigos se dirigía a hacer alpinismo y acamparon en Rusia. Poco despu&esacute; desaparecieron y dejaron sus carpas vacías. Cuando los cuerpos se encontraron llevaban poca ropa, tenían heridas internas y el cuerpo teñido de naranja. No había señales de lucha, aunque a algunos de ellos les faltaba la lengua y los ojos. Hasta el día de hoy no hay información sobre qué pudo haberles sucedido, aunque existen toda clase de teorías.
+<a href="9.jpg"><center><img src="9.jpg"></center>
+<tr>
+<tr>
+<td><center><h3><font color="blue">
+<center><h3><font color="white"></p></h3><font color="white"></center>
+
+
+</p></h2></center>
+<center><table border="05" width="100%" background="4.jpe" bgcolor="000000"><tr>
+<center><h2><font color="lifth blue">también tenemos diferentes historias de terror:</p></h1></center>
+<h3><a href="historias reales"><font color="red" face="Lucida Calligraphy"><img alt="Historias reales"</font></h2>
+<h3><font color="yellow" face="Lucida Calligraphy"><img alt="Historia reales de terror"</font></h2>
+<h3><font color="green" face="Lucida Calligraphy"><img alt="Historia de terror escalofriantes"</font></h2>
+<h3><font color="red" face="Lucida Calligraphy"><img alt="y muchos mas contenidos"</font></h2></td> 
+<td><script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script> 
+<script> 
+new TWTR.Widget({ 
+version: 2, 
+type: 'search', 
+search: 'software,informatica', 
+interval: 30000, 
+title: 'sigueme en twiter!', 
+subject: '@dddmmmccc', 
+width: 250, 
+height: 300, 
+theme: { 
+shell: { 
+dddmmmccc: '#34b9f7', 
+color: '#ffffff' 
+}, 
+tweets: { 
+dddmmmccc: '#ffffff', 
+color: '#000000', 
+links: '#3043bf' 
+} 
+}, 
+features: { 
+scrollbar: false, 
+loop: true, 
+live: true, 
+behavior: 'default' 
+} 
+}).render().start(); 
+</script><font color="orange"><center><table border="05" width="100%" bgcolor="white"><tr>:PARA MAS INFORMACION ELEGUIR LAS SIGUIENTES OPCIONES:</td> 
+
+</table></center> 
+
+
+<center> 
+<a href="idex.html"><font color="white/black"><img alt="INICIO"></a>
+<a href="servicios.html"><font color="white/black"><img alt="CELULAR: 967927491"></a>
+<a href="celular.html"><font color="white/black"><img alt="CONTACTOS"></a>
+<a href="descargas.html"><font color="white/black"><img alt="DESCARGAS"></a>
+ <br><h4><font color="blue" face="bell mt"><font color="white/black">*MANDANOS UN MENSAJE VIA EMAIL: 
+<a href="david_krishna@hotmail.com"><font color="white/black"><img alt="david_krishna@hotmail.com"></a></font></h4> 
+<a href="vive la vida no dejes que la vida te viva PUTOS"><font color="white/black"><img alt="VIVE LA VIDA NO DEJES QUE LA VIDA TE VIVA  SONRRIA DE ESO SE TRATA TU EXISTENCIA ARRIESGATE SI QUIERES GANAR">
+</center> 
+
+
+</body> 
+
+</html>
+<center><table border="05" width="100%" bgcolor="white"><tr>
+
+<html>
+<body>
+<video controls="controls" widht="200" height="600">
+<source src="13.mp4" type="video/mp4">
+<source src="13.mp4" type="video/webm">
+</video>
+</body>
+</html>
